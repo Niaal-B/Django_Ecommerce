e007d33f9f79ef5eb680c0df72704fc52e6760b5
Niaal-B <122875113+Niaal-B@users.noreply.github.com>
2025-01-04 14:22:39 +0530

    Update models.py

--- Cart/models.py
+++ Cart/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from Products.models import Product,SizeVariant
 
-
+ 
 class Cart(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
