f2eeb130e27d9786733ce81e7aaf1c5d4d5b7579
Niaal-B <nihnihal504@gmail.com>
2026-01-22 00:45:05 +0530

    Fix: Explicitly set Cloudinary storage on ImageField models to ensure images upload to Cloudinary

--- Categories/models.py
+++ Categories/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.core.files.storage import default_storage
+from django.conf import settings
+
+def get_cloudinary_storage():
+    """Get Cloudinary storage if configured, otherwise use default"""
+    storage_class = getattr(settings, 'DEFAULT_FILE_STORAGE', None)
+    if storage_class and 'cloudinary' in storage_class.lower():
+        from cloudinary_storage.storage import MediaCloudinaryStorage
+        return MediaCloudinaryStorage()
+    return default_storage
 
 class Category(models.Model):
     category_name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     is_listed = models.BooleanField(default=False)
-    # Storage will use DEFAULT_FILE_STORAGE from settings (Cloudinary if configured)
-    image = models.ImageField(upload_to='category_images/', blank=True, null=True) 
+    # Explicitly use Cloudinary storage if configured
+    image = models.ImageField(upload_to='category_images/', blank=True, null=True, storage=get_cloudinary_storage) 
     def __str__(self):
         return self.category_name
--- Products/models.py
+++ Products/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.core.files.storage import default_storage
+from django.conf import settings
 from Categories.models import Category
+
+def get_cloudinary_storage():
+    """Get Cloudinary storage if configured, otherwise use default"""
+    storage_class = getattr(settings, 'DEFAULT_FILE_STORAGE', None)
+    if storage_class and 'cloudinary' in storage_class.lower():
+        from cloudinary_storage.storage import MediaCloudinaryStorage
+        return MediaCloudinaryStorage()
+    return default_storage
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -9,10 +18,10 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=7, decimal_places=2)  # Adjusted precision
     offer = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)  # Increased precision
     color = models.CharField(max_length=50, null=True, blank=True)
-    # Storage will use DEFAULT_FILE_STORAGE from settings (Cloudinary if configured)
-    image1 = models.ImageField(upload_to='products/', null=True)
-    image2 = models.ImageField(upload_to='products/', null=True, blank=True)
-    image3 = models.ImageField(upload_to='products/', null=True, blank=True)
+    # Explicitly use Cloudinary storage if configured
+    image1 = models.ImageField(upload_to='products/', null=True, storage=get_cloudinary_storage)
+    image2 = models.ImageField(upload_to='products/', null=True, blank=True, storage=get_cloudinary_storage)
+    image3 = models.ImageField(upload_to='products/', null=True, blank=True, storage=get_cloudinary_storage)
     created_at = models.DateTimeField(auto_now_add=True)
     is_listed = models.BooleanField(default=False)
 
