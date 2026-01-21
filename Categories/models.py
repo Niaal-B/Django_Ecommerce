44f8b51ad97bd86f7e43994f3f840d80bdee04f1
Niaal-B <nihnihal504@gmail.com>
2026-01-22 02:39:17 +0530

    test deploy

--- Categories/models.py
+++ Categories/models.py
@@ -14,7 +14,6 @@ class Category(models.Model):
     category_name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     is_listed = models.BooleanField(default=False)
-    # Explicitly use Cloudinary storage if configured
     image = models.ImageField(upload_to='category_images/', blank=True, null=True, storage=get_cloudinary_storage) 
     def __str__(self):
         return self.category_name
